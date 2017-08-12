e29e1f3bd117f98a59cab8cab4cf82f3c861b365
Angel Ortiz <angelo12@vt.edu>
2017-08-12 17:06:35 -0500

    Fixing minor issues on scanner script behaviour

--- selfDrivingSim/Assets/Scripts/Scanner.cs
+++ selfDrivingSim/Assets/Scripts/Scanner.cs
@@ -33,7 +33,7 @@ public class Scanner : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         scansPerSecond = calculateScansPerSecond();
 
 	}
